6713aa8c3ef286f9f7ce56df6c8f1a6c60e8481e
Mahmoud Abdelbary <mahmoud.abelbaty@gmail.com>
2018-04-30 01:49:35 +0200

    30/4

--- untitled0.py
+++ untitled0.py
@@ -1,62 +1,117 @@
 import json
+import queue
 import time
-jsonData = open('task.json').read()   
-respond = json.loads(jsonData)
-serverTasks = None
+
+taskList = []
+gate = queue.Queue()
+
+
 columns = 10
 rows = 10
 xout = 9
 yout = 9
 zout = 9
-gate = []
 
+#initiate gate positions
 for i in range(10):
     for j in range(10):
-        gate.append((0,i,j))
+        gate.put((0,i,j))
 
-print(len(gate))
-
-if respond['success'] == True:
-    serverTasks = list(respond['result'])
-    print(type(serverTasks))
-
-    while serverTasks:
+def sendTasksToAVR(packages):
+    print(packages)
     
-        Tasks = list(serverTasks)
-        for task in Tasks:
-            tempTask = {}
-            
-            tempTask['id']  = task['id']
     
-            if task['isOut'] == True:
-                tempTask['xSt'] = task['x']
-                tempTask['ySt'] = task['y']
-                tempTask['zSt'] = task['z']
-                
-                tempTask['xEn'] = xout
-                tempTask['yEn'] = yout
-                tempTask['zEn'] = zout
-                serverTasks.remove(task)
+    
+def excuiteTasks():
+    todo = list(taskList)
+    taskcnt = len(todo)
+    taskStrings = []
+    packages = []
+    
+    #transform each task to a string
+    for i in range(taskcnt):
+        task = todo[i]
+        t=""
+        for k in task:
+            t +=  str(task[k]) if task[k] > 9 else "0" +str(task[k])
+            t += "/"
+        t = t[:-1]
+        taskStrings.append(t)
+        taskcnt-=1
             
-            elif len(gate):
-                tempTask['xSt'] = gate[0][0]
-                tempTask['ySt'] = gate[0][1]
-                tempTask['zSt'] = gate[0][2]
-                
-                tempTask['xEn'] = task['x']
-                tempTask['yEn'] = task['y']
-                tempTask['zEn'] = task['z']
-                
-                gate.pop(0)
-                serverTasks.remove(task)
-            else :
-                time.sleep(1)
-                
-            print(tempTask)
+    print(taskStrings)
+    
+    tempTaskStrings = list(taskStrings)
+    #form packages
+    while len(tempTaskStrings):
+        packagecnt = min(len(tempTaskStrings),3)
+        package  = ""
+        package += "0"+str(packagecnt)+"/"
+        
+        for i in range(packagecnt):
+           package +=  tempTaskStrings[0] + "/"
+           tempTaskStrings.pop(0)
+        package = package[:-1] + "#"
+        packages.append((packagecnt,package))
+    sendTasksToAVR(packages)
+    
+    
+#read data from server
+jsonData = open('task.json').read()  
+respond = json.loads(jsonData)
 
-print(len(serverTasks))
-print(len(gate))
-print(respond['result'])            
+#repeat till sucess response
+while respond['success'] == False:
+    jsonData = open('task.json').read()  
+    respond = json.loads(jsonData)
+
+#duplicate server Tasks to work on them
+serverTasks = list(respond['result'])
+
+#repate until finish all serverTasks
+while serverTasks:
+
+    Tasks = list(serverTasks)
+    for task in Tasks:
+        tempTask = {}
+        
+        tempTask['id']  = task['id']
+
+        if task['isOut'] == True:
+            tempTask['xSt'] = task['x']
+            tempTask['ySt'] = task['y']
+            tempTask['zSt'] = task['z']
+            
+            tempTask['xEn'] = xout
+            tempTask['yEn'] = yout
+            tempTask['zEn'] = zout
+            taskList.append(tempTask)
+            serverTasks.remove(task)
+        
+        elif gate.empty() is False:
+            g = gate.get()
+            tempTask['xSt'] = g[0]
+            tempTask['ySt'] = g[1]
+            tempTask['zSt'] = g[2]
+            
+            tempTask['xEn'] = task['x']
+            tempTask['yEn'] = task['y']
+            tempTask['zEn'] = task['z']
+            taskList.append(tempTask)
+            serverTasks.remove(task)
+            
+        #if the task is input and the gate is avialbe pos is empty 
+        else :
+            continue
+            
+    if len(serverTasks) == 0 or gate.empty is True:
+            print('--Tasks loaded on gate & Ready To be Excuited--')
+            
+print('--Tasks recived form server FINISHED--')
+excuiteTasks()
+
+
+print(taskList)     
             
         
 
